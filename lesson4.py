c7c0dd3f583df4f6ebd09924136ab019d512d597
Eugene <polish@i.ua>
2017-10-27 22:56:22 +0300

    init

---
+++ lesson4.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Калькулятор
+first_number = float(input('Введите первое число: '))
+second_number = float(input('Введите второе число: '))
+
+print('Сложение:\t', first_number + second_number)
+print('Вычитание:\t', first_number - second_number)
+print('Умножение:\t', first_number * second_number)
+print('Деление:\t', first_number * second_number)
+
+''' Это многострочный комментарий
+  TODO (задание)
+  1) Добавить свои функции print() с операциями из файла lesson2.py
+  2) Исправить ошибки в программе если они есть
+'''
