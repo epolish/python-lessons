fb8f483f7f4725af8642be67060556b153424c32
Eugene <polish@i.ua>
2018-01-06 13:58:55 +0200

    Делать задание в файле app.py

--- zimnije_kanikuly/app.py
+++ zimnije_kanikuly/app.py
@@ -1,6 +1,8 @@
 from request import check_password
 
 '''
+Установка: Скачать все 3 файлика (app.py, passwords.txt, request.py) в одну папку все вместе.
+
 Задача: методом полного перебора (brute force)
 подобрать пароль к странице на сайте http://python-lessons.zzz.com.ua
 
